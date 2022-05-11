af5b89e07f5679dedf18c1d12e06fc9964ed0811
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-05-11 16:13:41 +0600

    Server Connect with Database

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-const express = require('express')
-const app = express()
-const cors = require('cors');
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctoscluster.6eoyi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const appointCollection = client
+      .db("DoctorsPortal")
+      .collection("appointments");
+
+      // Get API
+    app.get("/appointments", async (req, res) => {
+      const result = await appointCollection.find().toArray();
+      res.send(result);
+    });
+  } finally {
+    // Nothing Here
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
