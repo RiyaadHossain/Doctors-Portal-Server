0bfb21a9d862c0800984cb71e3d6d4b5863e87e9
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-05-15 12:57:36 +0600

    verify token and unauthorized access set up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -13,9 +13,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctoscluster.6eoyi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
-const func = () => {
-
-}
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+   
+    next();
+  });
+};
 
 async function run() {
   try {
@@ -35,13 +47,15 @@ async function run() {
     });
 
     // Get API
-    app.get("/booking", async (req, res) => {
+    app.get("/booking", verifyToken, async (req, res) => {
       const patientEmail = req.query.patient;
-      const patientToken = req.headers.authorization
-      console.log(patientToken);
-      const query = { patientEmail : patientEmail};
-      const result = await bookingCollection.find(query).toArray();
-      res.send(result);
+      const patientToken = req.decoded.email;
+      if (patientEmail === patientToken) {
+        const query = { patientEmail: patientEmail };
+        const result = await bookingCollection.find(query).toArray();
+        return res.send(result);
+      }
+      res.status(401).send({ message: "Forbidden Request" });
     });
 
     // Discalimer: It's not the accurate way to query. Learn - Aggregate, Lookup, Pipeline, Match, Group (MongoDB)
@@ -79,9 +93,9 @@ async function run() {
         updatedDoc,
         options
       );
-      const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN)
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN);
       console.log(token);
-      res.send({result, token});
+      res.send({ result, token });
     });
 
     // Post API
